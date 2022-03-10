92aab70007308b1c1ed7c4e0cb2e852d56fd78b1
JasonChuanTian <tianchuanjason@gmail.com>
2022-03-10 00:22:58 -0800

    simplfy

--- src/amazon_framework/State.java
+++ src/amazon_framework/State.java
@@ -9,51 +9,39 @@ public class State {
     public StateArray childStates = null;
     public final double gamma = 0.80;
 
-    public synchronized void backPropagation(boolean win, int steps)
-    {
+    public synchronized void backPropagation(boolean win, int steps) {
         if (parentState == null)
             return;
 
         if (is_op == false) {
-            if (win == true){
+            if (win == true) {
                 value += 1 * Math.pow(gamma, steps);
                 evalue -= 1 * Math.pow(gamma, steps);
-                if (value < 0)
-                    value = 0;
-                if (evalue < 0)
-                    evalue = 0;
                 if (parentState != null)
                     parentState.backPropagation(win, ++steps);
             } else {
                 value -= 1 * Math.pow(gamma, steps);
                 evalue += 1 * Math.pow(gamma, steps);
-                if (value < 0)
-                    value = 0;
-                if (evalue < 0)
-                    evalue = 0;
                 if (parentState != null)
                     parentState.backPropagation(win, ++steps);
             }
         } else {
-            if (win == true){
+            if (win == true) {
                 value -= 1 * Math.pow(gamma, steps);
                 evalue += 1 * Math.pow(gamma, steps);
-                if (value < 0)
-                    value = 0;
-                if (evalue < 0)
-                    evalue = 0;
                 if (parentState != null)
                     parentState.backPropagation(win, ++steps);
             } else {
                 value += 1 * Math.pow(gamma, steps);
                 evalue -= 1 * Math.pow(gamma, steps);
-                if (value < 0)
-                    value = 0;
-                if (evalue < 0)
-                    evalue = 0;
                 if (parentState != null)
                     parentState.backPropagation(win, ++steps);
             }
         }
+        
+        if (value < 0)
+            value = 0;
+        if (evalue < 0)
+            evalue = 0;
     }
 }
